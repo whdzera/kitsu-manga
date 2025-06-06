9dfba477a83cab3e9098b31e7e3604e1476b70eb
rokhimin <rokhim.whd@gmail.com>
2025-06-06 17:32:50 +0700

    validation comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :chapter
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 10, maximum: 700 }
+
+  private
 end
