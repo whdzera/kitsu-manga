274f1ccdf589bb22ba40ead4d3470bb5f7a056ba
rokhimin <rokhim.whd@gmail.com>
2025-06-07 14:28:52 +0700

    change link manga from %20 to -

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -1,10 +1,11 @@
 class MangasController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :by_genre, :genres, :search] 
-  before_action :require_admin, only: [:create, :new, :update, :edit, :destroy]
-  before_action :set_manga, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!,
+                except: %i[index show by_genre genres search]
+  before_action :require_admin, only: %i[create new update edit destroy]
+  before_action :set_manga, only: %i[show edit update destroy]
 
   def to_param
-    title.parameterize 
+    title.parameterize
   end
 
   def index
@@ -12,7 +13,7 @@ class MangasController < ApplicationController
   end
 
   def show
-    @chapters = @manga.chapters.order(chapter_number: :desc) 
+    @chapters = @manga.chapters.order(chapter_number: :desc)
   end
 
   def new
@@ -27,11 +28,15 @@ class MangasController < ApplicationController
 
     respond_to do |format|
       if @manga.save
-        format.html { redirect_to @manga, notice: "Manga was successfully created." }
+        format.html do
+          redirect_to @manga, notice: "Manga was successfully created."
+        end
         format.json { render :show, status: :created, location: @manga }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @manga.errors, status: :unprocessable_entity }
+        format.json do
+          render json: @manga.errors, status: :unprocessable_entity
+        end
       end
     end
   end
@@ -39,11 +44,15 @@ class MangasController < ApplicationController
   def update
     respond_to do |format|
       if @manga.update(manga_params)
-        format.html { redirect_to @manga, notice: "Manga was successfully updated." }
+        format.html do
+          redirect_to @manga, notice: "Manga was successfully updated."
+        end
         format.json { render :show, status: :ok, location: @manga }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @manga.errors, status: :unprocessable_entity }
+        format.json do
+          render json: @manga.errors, status: :unprocessable_entity
+        end
       end
     end
   end
@@ -52,15 +61,19 @@ class MangasController < ApplicationController
     @manga.destroy!
 
     respond_to do |format|
-      format.html { redirect_to mangas_path, status: :see_other, notice: "Manga was successfully destroyed." }
+      format.html do
+        redirect_to mangas_path,
+                    status: :see_other,
+                    notice: "Manga was successfully destroyed."
+      end
       format.json { head :no_content }
     end
   end
 
   def by_genre
     @genre = params[:name]
-    @mangas = Manga.where("genre LIKE ?", "%#{@genre}%").page(params[:page]).per(6)
-
+    @mangas =
+      Manga.where("genre LIKE ?", "%#{@genre}%").page(params[:page]).per(6)
   end
 
   def genres
@@ -69,31 +82,41 @@ class MangasController < ApplicationController
 
   def search
     query = params[:q]
-    @mangas = if query.present?
-                Manga.where("title LIKE ?", "%#{query}%").page(params[:page]).per(6)
-              else
-                Manga.all.page(params[:page]).per(6)
-              end
-
-    respond_to do |format|
-      format.html 
+    @mangas =
+      if query.present?
+        Manga.where("title LIKE ?", "%#{query}%").page(params[:page]).per(6)
+      else
+        Manga.all.page(params[:page]).per(6)
       end
+
+    respond_to { |format| format.html }
   end
 
   private
 
   def manga_params
-    params.require(:manga).permit(:title, :alternative_title, :status, :manga_type, 
-                                  :series, :author, :rating, :created_date, 
-                                  :genre, :image_cover, :synopsis)
+    params.require(:manga).permit(
+      :title,
+      :alternative_title,
+      :status,
+      :manga_type,
+      :series,
+      :author,
+      :rating,
+      :created_date,
+      :genre,
+      :image_cover,
+      :synopsis
+    )
   end
 
   def set_manga
-    @manga = if params[:id].to_i.to_s == params[:id]
-               Manga.find(params[:id])
-             else
-               Manga.find_by!(title: params[:id].tr('-', ' '))
-             end
+    @manga =
+      if params[:id].to_i.to_s == params[:id]
+        Manga.find(params[:id])
+      else
+        Manga.find_by!(title: params[:id].tr("-", " "))
+      end
   end
 
   def require_admin
@@ -102,5 +125,4 @@ class MangasController < ApplicationController
       redirect_to root_path
     end
   end
-  
 end
